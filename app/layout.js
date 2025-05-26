0d31cc33c91d8b7bc8f2cf97ae74e3650817757a
Andrei Ioan-Sebastian <sebiandrei28@gmail.com>
2025-05-26 14:32:12 +0300

    Image optimisation for Logo component

--- app/layout.js
+++ app/layout.js
@@ -23,7 +23,9 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body
         className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col antialiased`}>
+
         <Header />
+        
         <div className="flex-1 px-8 py-12">
           <main className="max-w-7xl mx-auto">{children}</main>
         </div>
