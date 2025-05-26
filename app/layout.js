415c2f7631a4d4badc640b38576891fc8c287e48
Andrei Ioan-Sebastian <sebiandrei28@gmail.com>
2025-05-26 15:07:31 +0300

    Build Home page and added bg img

--- app/layout.js
+++ app/layout.js
@@ -22,10 +22,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col antialiased`}>
-
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col antialiased relative`}>
         <Header />
-        
+
         <div className="flex-1 px-8 py-12">
           <main className="max-w-7xl mx-auto">{children}</main>
         </div>
