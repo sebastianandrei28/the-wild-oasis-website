f3a164431166c8f9e31240d3f4171b343c444be0
Andrei Ioan-Sebastian <sebiandrei28@gmail.com>
2025-05-30 13:06:21 +0300

    Added CabinList and suspense for a better loading experience

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -7,8 +7,8 @@ export async function getCabin(id) {
   try {
     const { data, error } = await supabase
       .from("cabins")
-      .select("*")
-      .order("id");
+      .select("id, name, maxCapacity, regularPrice, discount, image")
+      .order("name");
 
     if (error) {
       console.error(error);
--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,14 +1,11 @@
-import Counter from "@/app/_components/Counter";
-import CabinCard from "@/app/_components/CabinCard";
-import { getCabin } from "../_lib/data-service";
+import { Suspense } from "react";
+import CabinList from "../_components/CabinList";
+import Spinner from "../_components/Spinner";
 
 export const metadata = {
   title: "Cabins",
 };
-export default async function Page() {
-  // CHANGE
-  const cabins = await getCabin();
-
+export default function Page() {
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -22,14 +19,9 @@ export default async function Page() {
         away from home. The perfect spot for a peaceful, calm vacation. Welcome
         to paradise.
       </p>
-
-      {cabins.length > 0 && (
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-          {cabins.map((cabin) => (
-            <CabinCard cabin={cabin} key={cabin.id} />
-          ))}
-        </div>
-      )}
+      <Suspense fallback={<Spinner />}>
+        <CabinList />
+      </Suspense>
     </div>
   );
 }
